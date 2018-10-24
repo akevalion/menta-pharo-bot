ee4cbe3243c06c6add4cc2baf3ed7a543e95fa63
Martin Dias <tinchodias@gmail.com>
2018-10-24 16:55:44 -0300

    fix build script

--- report-server/build.sh
+++ report-server/build.sh
@@ -3,13 +3,16 @@
 # See what is executed and fail on bad exit codes
 set -ex
 
+BASEDIR=$(pwd)
+REPO="filetree://$BASEDIR/src"
+
+# ensure a clean build directory
+rm -rf build
 mkdir build
 cd build
 
 # download image and vm
 curl get.pharo.org/70+vm | bash
 
-REPO="filetree://src"
-./pharo Pharo.image metacello install $REPO BaselineOfMenta #--install=development
-#./pharo Pharo.image eval --save '(IceRepositoryCreator new repository: nil; location: FileSystem workingDirectory; createRepository) register'
-#./pharo Pharo.image eval --save "Gofer it smalltalkhubUser: 'zeroflag' project: 'Teapot'; configuration; loadStable"
+# load code
+./pharo Pharo.image metacello install $REPO BaselineOfMenta
