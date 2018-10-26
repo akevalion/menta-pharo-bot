6860ae341fbaf5c314f9abc36cb429de7e1c8fc3
Martin Dias <tinchodias@gmail.com>
2018-10-26 12:40:07 -0300

    use request-promise + other aesthetic tweaks

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 
-var request = require("request");
-var rp = require('request-promise')
+const rp = require('request-promise')
 
 // Pharo report-server should listen here
 const reportServerBaseUrl = "http://localhost:8083/"
 
 /**
- * This is the entry point for your Probot App.
+ * This is the entry point for the Github's Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
 module.exports = app => {
 
-  app.log('App loaded!')
+  app.log('Github App ready!')
 
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
@@ -19,7 +18,6 @@ module.exports = app => {
   })
 
   app.on('issues.edited', async context => {
-
     //console.log(context.payload);
 
     const options = {
@@ -30,25 +28,20 @@ module.exports = app => {
       json:true
     }
 
-// TODO: convert to rp and usetest when pharo is down
-
-    return request(options, function (error, response, body) {
-      if (error) { return console.log(error) }
-
-      console.log(body)
-
-      const issueComment = context.issue({ body: body })
-      return context.github.issues.createComment(issueComment)
-    })
+    return rp(options)
+      .then(body => {
+        console.log(body)
+        const issueComment = context.issue({ body: body })
+        return context.github.issues.createComment(issueComment)
+      })
   })
 
   // Get an express router to expose new HTTP endpoints (a liveness check)
   app.route('/alive').get('/', (req, res) => {
-
-    console.log("I'm alive, so let's ask to report server.")
-
-    return rp(reportServerBaseUrl + 'alive').then(body => res.end(body)).catch(_ => res.sendStatus(500))
+    // console.log("I'm alive, so let's ask to report server.")
+    return rp(reportServerBaseUrl + 'alive')
+      .then(body => res.end(body))
+      .catch(_ => res.send.status(500).end("Report server is not alive"))
   })
-
  
 }
