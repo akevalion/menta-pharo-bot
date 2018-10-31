6906913e087dc1d4db657cc9cce70dd7739ff8a9
Martin Dias <tinchodias@gmail.com>
2018-10-31 16:15:28 -0300

    forgot to update start script

--- report-server/start.sh
+++ report-server/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./build/pharo build/Pharo.image eval --no-quit "MeServer new start"
+./build/pharo build/Pharo.image eval --no-quit "\
+MeDemo new\
+	basePublicUrl: 'http://gate.dcc.uchile.cl:3000/public/';\
+	basePublicPath: (FileLocator imageDirectory parent parent / 'public') asPath;\
+	start"
