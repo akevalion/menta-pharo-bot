92c9c4b549e5062e32c5a377c7605e1492e99d35
Martin Dias <tinchodias@gmail.com>
2018-11-28 12:07:29 -0300

    generate the JWT with the received arguments (remove dotenv)

--- jwt-token-generator/index.js
+++ jwt-token-generator/index.js
@@ -1,12 +1,19 @@
-const path = require('path')
-require('dotenv').config({ path: path.join(__dirname, '../.env') })
+const fs = require('fs')
 const jwt = require('jsonwebtoken')
 
-const cert = process.env.PRIVATE_KEY
-const appId = process.env.APP_ID
+if (!process.argv[2] | !process.argv[3]) {
+  console.log("Generates a JWT signed with RS256 algorithm as Github API expects for Github Apps\n")
+  console.log("Expected arguments: <app_id> <pem_path>\n")
+  console.log("     <app_id>: The integer that identifies the Github App.")
+  console.log("   <pem_path>: The path to the pem file with the RSA private key of the Github App.")
+  return 1;
+}
 
-let now = Math.floor(Date.now() / 1000)
-let payload = { "iat": now, "exp": now + (10 * 60), "iss": appId }
+const appId = parseInt(process.argv[2])
+const cert = fs.readFileSync(process.argv[3])
 
-let token = jwt.sign(payload, cert, { algorithm: 'RS256'})
+const now = Math.floor(Date.now() / 1000)
+const payload = { "iat": now, "exp": now + (10 * 60), "iss": appId }
+
+const token = jwt.sign(payload, cert, { algorithm: 'RS256'})
 console.log(token)
