ff3450421044f73dbf59cfd90e232088518b3ff1
Martin Dias <tinchodias@gmail.com>
2018-12-19 20:31:29 -0300

    added VNC server in start script

--- report-server/start.sh
+++ report-server/start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-./build/pharo build/Pharo.image eval --no-quit "(MeEasy readFromStonFileAt: Menta repositoryPath / 'MeDemoApp.ston') start"
+./build/pharo build/Pharo.image eval --no-quit "\
+(MeEasy readFromStonFileAt: Menta repositoryPath / 'MeDemoApp.ston') start.\
+RFBServer current\
+    initializePreferences;\
+    allowEmptyPasswords: false;\
+    setFullPassword: 'secret';\
+    setViewPassword: 'secret';\
+    enableLogging: true;\
+    enableDebugging: true;\
+    start: 0."
