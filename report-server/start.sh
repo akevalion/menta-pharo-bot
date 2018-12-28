549a41a8e333c65aab4b5d35bfd652189a179c54
Martin Dias <tinchodias@gmail.com>
2018-12-28 13:08:33 -0300

    extract pharo code from start.sh

--- report-server/start.sh
+++ report-server/start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-./build/pharo build/Pharo.image eval --no-quit "\
-UIManager default: NonInteractiveUIManager new.\
-(MeEasy readFromStonFileAt: Menta repositoryPath / 'MeDemoApp.ston') start.\
-RFBServer current\
-    initializePreferences;\
-    allowEmptyPasswords: false;\
-    setFullPassword: 'secret';\
-    setViewPassword: 'secret';\
-    enableLogging: true;\
-    enableDebugging: true;\
-    start: 0."
+./build/pharo build/Pharo.image eval --no-quit start.st
