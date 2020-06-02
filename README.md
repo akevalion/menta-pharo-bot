12d6e2d3812ffb9fbc9610a54ba64663ba02f133
Martín Dias <tinchodias@gmail.com>
2020-06-02 12:05:46 -0400

    Remove nodejs scripts

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Menta
 <img src="assets/mint.png" alt="logo" width="100"/>
 
-> A GitHub App built with [Pharo](https://pharo.org/) to experiment with Github API and webhooks.
+> A small [Pharo](https://pharo.org/) example/framework that start creating your own GitHub App that interacts with GitHub via their APIs and webhooks.
 
 ## How to use
 
-The `build_all.sh ` script downloads a Pharo image and loads the code and it's dependencies. 
+The `server/build.sh ` script downloads a Pharo image, loads the code and it's dependencies. 
 
-To start the HTTP server, you should read class comments of MeGithubApp and subclasses.
+Setup your Github app with and id and PEM private key. Read class comments of `MeGithubApp` and subclasses to learn about it.
 
 The `server/start.sh` script starts the Pharo image in headless mode. The image will start two servers: a HTTP server (3000 by default) and a VNC server (5900, the default port).
 
--- server/build.sh
+++ server/build.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-# See what is executed and fail on bad exit codes
-set -ex
+# We want to fail on bad exit codes
+set -e
 
 BASEDIR=$(pwd)
-REPO="filetree://$BASEDIR/src"
+REPO="tonel://$BASEDIR/src"
 
-# ensure a clean build directory
+echo ""
+echo " ---------------------------------"
+echo "| Ensure a clean build/ directory |"
+echo " ---------------------------------"
+echo ""
 rm -rf build
 mkdir build
 cd build
 
-# download image and vm
+echo ""
+echo " -------------------"
+echo "| Downloading Pharo |"
+echo " -------------------"
+echo ""
 curl https://get.pharo.org/64/80+vm | bash
 
-# load code
+echo ""
+echo " -------------------------------"
+echo "| Loading code and dependencies |"
+echo " -------------------------------"
+echo ""
 ./pharo Pharo.image metacello install $REPO BaselineOfMenta
+
+echo ""
+echo " -------------------------"
+echo "| Build Finished: SUCCESS |"
+echo " -------------------------"
+echo ""
+echo "Run './start.sh' to start the server."
+exit 0
